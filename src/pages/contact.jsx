13916e5cf0e096bdd47687037fd7e70e3b5f5404
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-12-10 17:42:29 +0500

    routes done

---
+++ src/pages/contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className='text-3xl'>Contact</div>
+  )
+}
+
+export default Contact
---
+++ src/pages/project.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Project() {
+  return (
+    <div className='text-3xl'>Project</div>
+  )
+}
+
+export default Project
