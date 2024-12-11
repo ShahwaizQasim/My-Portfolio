bf186b48575d3abda75247e84d269e1ea76925df
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-12-11 13:12:10 +0500

    animation library install

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import './index.css'
 import './App.css'
+import Aos from "aos";
+import "aos/dist/aos.css";
 import Home from './pages/home'
 import About from './pages/about'
 import Contact from './pages/contact'
 import Project from './pages/project'
 import Navbar from './components/navbar'
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    Aos.init();
+  }, []);
 
   return (
     <BrowserRouter>
