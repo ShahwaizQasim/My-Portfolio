94f299714e43ad3d9b615ea6515b94b65f3edb08
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-12-23 11:10:43 +0500

    type.js install

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
-import heroImg from '../assets/hero-img.png'
+import React, { useEffect } from 'react'
+// import heroImg from '../assets/hero-img.png'
+import myImg from '../assets/myImg.jfif'
+import Typed from 'typed.js';
 
 function HeroSection() {
+  useEffect(() => {
+    // Typing Effect
+    const typed = new Typed(".typedText", {
+      strings: ["Syed Shahwaiz Qasim", "Full Stack Developer", "Freelancer"],
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 80,
+      backDelay: 2000,
+    })
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <section className="hero" id="hero">
       <div
@@ -10,12 +26,17 @@ function HeroSection() {
         <div className="container">
           <div className="row pt-5">
             <div className="col-lg-6 col-md-12 col-sm-12 mt-5 mb-5">
-              <div className="dis">
-                <p className="hdg1" style={{paddingTop:'70px', fontSize:'0.8rem', textTransform:"uppercase"}}>Welcome to my World</p>
+              <div className="dis1">
+                <p className="hdg animate__animated animate__fadeInUp" style={{
+                  paddingTop: '90px', fontSize: '0.8rem', textTransform: "uppercase", fontWeight: '600'
+                }}>Welcome to my World</p>
                 <h1
-                  className="mt-3 hdg animate__animated animate__fadeInUp animate__delay-1s"
+                  className="mt-3 hdg animate__animated animate__fadeInUp animate__delay-1s" style={{
+                    fontWeight: "normal",
+                    fontSize: "2.7rem"
+                  }}
                 >
-                  Hi, I am Syed Shahwaiz Qasim
+                  Hi, I am <span className="typedText"></span>
                 </h1>
                 <h5 className="mt-3 hdg1 animate__animated animate__fadeInUp animate__delay-2s">
                   These Is Correct Platform To Design Your Website
@@ -32,13 +53,14 @@ function HeroSection() {
             </div>
             <div className="col-lg-6 col-md-12 col-sm-12  mt-5 mb-5">
               <div className="dis mt-5 mb-5 animate__animated animate__zoomIn">
-                <img
-                  src={heroImg}
+                {/* <img
+                  src={myImg}
                   className="img-fluid"
                   height="100%"
                   width="100%"
                   alt=""
-                />
+                /> */}
+                <div className="myImg"></div>
               </div>
             </div>
           </div>
