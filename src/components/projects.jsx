f18785a2d541f8bae3eb9c9f82a4203a4b48760f
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-12-23 16:16:19 +0500

    project components in working process

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { Link } from 'react-router';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLink } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { Link } from 'react-router'
 
 function Projects() {
 
@@ -26,25 +29,37 @@ function Projects() {
                                     <div className="flip-box-inner">
                                         <div
                                             className="flip-box-front"
-                                            style={{ border: "1px solid #CE9160" }}
                                         >
                                             <h4 className="hdg2 mt-4">GrandWriting Website</h4>
                                             <center>
-                                                <h6 className="hdg1 pt-2">
-                                                    Architect Website Design By ShahwaizQasim.
+                                                <h6 className="hdg1 pt-2" style={{
+                                                    fontWeight: '400',
+                                                    paddingInline: "30px"
+                                                }}>
+                                                    A user-friendly website designed to assist individuals and organizations in creating and managing grant applications.
+                                                    Built with HTML, CSS, and JavaScript, the site provides an intuitive interface for grant writing, submission
+                                                    tracking, and resource management
                                                 </h6>
                                             </center>
-                                            <button className="btn4 mt-2">View Website</button>
-                                        </div>
-                                        <div
-                                            className="flip-box-back"
-                                            style={{ backgroundImage: "url(Assets/img/img2.png)" }}
-                                        >
-                                            <Link to="https://shahwaizqasim.github.io/Architect-Website/" target="_blank">
-                                                <button className="btn2" style={{ marginTop: 80 }}>
-                                                    Click
-                                                </button>
-                                            </Link>
+                                            <div style={{
+                                                display: 'flex',
+                                                gap: '20px'
+                                            }}>
+                                                <Link to={'https://shahwaizqasim.github.io/GrandWriting-Website-Project/'} target={"_blank"}>
+                                                    <button className="btn4 mt-3 mb-4" style={{
+                                                        borderRadius: '5px'
+                                                    }}>
+                                                        Hosted Link <FontAwesomeIcon icon={faLink} className='ps-1' />
+                                                    </button>
+                                                </Link>
+                                                <Link to={'https://github.com/ShahwaizQasim/GrandWriting-Website-Project'} target={"_blank"}>
+                                                    <button className="btn4 mt-3 mb-4" style={{
+                                                        borderRadius: '5px'
+                                                    }}>
+                                                        Github Repo <FontAwesomeIcon icon={faGithub} className='ps-1' />
+                                                    </button>
+                                                </Link>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -58,7 +73,6 @@ function Projects() {
                                     <div className="flip-box-inner">
                                         <div
                                             className="flip-box-front"
-                                            style={{ border: "1px solid #CE9160" }}
                                         >
                                             <h4 className="hdg2 mt-4">NEOH Website</h4>
                                             <center>
@@ -68,19 +82,7 @@ function Projects() {
                                             </center>
                                             <button className="btn4 mt-2">View Website</button>
                                         </div>
-                                        <div
-                                            className="flip-box-back"
-                                            style={{ backgroundImage: "url(Assets/img/bg.jpg)" }}
-                                        >
-                                            <a
-                                                href="Assets/Projects/Web Project 2/index.html"
-                                                target="_blank"
-                                            >
-                                                <button className="btn2" style={{ marginTop: 80 }}>
-                                                    Click
-                                                </button>
-                                            </a>
-                                        </div>
+
                                     </div>
                                 </div>
                             </div>
@@ -93,7 +95,6 @@ function Projects() {
                                     <div className="flip-box-inner">
                                         <div
                                             className="flip-box-front"
-                                            style={{ border: "1px solid #CE9160" }}
                                         >
                                             <h4 className="hdg2 mt-4">Casa Hotel Website</h4>
                                             <center>
@@ -103,19 +104,7 @@ function Projects() {
                                             </center>
                                             <button className="btn4 mt-2">View Website</button>
                                         </div>
-                                        <div
-                                            className="flip-box-back"
-                                            style={{ backgroundImage: "url(Assets/img/hero-1.jpg)" }}
-                                        >
-                                            <a
-                                                href="Assets/Projects/Web Project 3/Index.html"
-                                                target="_blank"
-                                            >
-                                                <button className="btn2" style={{ marginTop: 80 }}>
-                                                    Click
-                                                </button>
-                                            </a>
-                                        </div>
+
                                     </div>
                                 </div>
                             </div>
@@ -128,7 +117,6 @@ function Projects() {
                                     <div className="flip-box-inner">
                                         <div
                                             className="flip-box-front"
-                                            style={{ border: "1px solid #CE9160" }}
                                         >
                                             <h4 className="hdg2 mt-4">Quiz Application</h4>
                                             <center>
@@ -139,19 +127,7 @@ function Projects() {
                                             </center>
                                             <button className="btn5 mt-2">View Application</button>
                                         </div>
-                                        <div
-                                            className="flip-box-back"
-                                            style={{ backgroundImage: "url(Assets/img/App 1.png)" }}
-                                        >
-                                            <a
-                                                href="Assets/Projects/Quiz Application/index.html"
-                                                target="_blank"
-                                            >
-                                                <button className="btn2" style={{ marginTop: 80 }}>
-                                                    Click
-                                                </button>
-                                            </a>
-                                        </div>
+
                                     </div>
                                 </div>
                             </div>
@@ -164,7 +140,6 @@ function Projects() {
                                     <div className="flip-box-inner">
                                         <div
                                             className="flip-box-front"
-                                            style={{ border: "1px solid #CE9160" }}
                                         >
                                             <h4 className="hdg2 mt-2">Login Form</h4>
                                             <center>
@@ -177,19 +152,7 @@ function Projects() {
                                             </center>
                                             <button className="btn5 mt-2">View Login Form</button>
                                         </div>
-                                        <div
-                                            className="flip-box-back"
-                                            style={{ backgroundImage: "url(Assets/img/App 2.png)" }}
-                                        >
-                                            <a
-                                                href="Assets/Projects/Login Form/Login.html"
-                                                target="_blank"
-                                            >
-                                                <button className="btn2" style={{ marginTop: 80 }}>
-                                                    Click
-                                                </button>
-                                            </a>
-                                        </div>
+
                                     </div>
                                 </div>
                             </div>
@@ -202,7 +165,6 @@ function Projects() {
                                     <div className="flip-box-inner">
                                         <div
                                             className="flip-box-front"
-                                            style={{ border: "1px solid #CE9160" }}
                                         >
                                             <h4 className="hdg2 mt-4">Calculator</h4>
                                             <center>
@@ -212,19 +174,7 @@ function Projects() {
                                             </center>
                                             <button className="btn5 mt-2">View Calculator</button>
                                         </div>
-                                        <div
-                                            className="flip-box-back"
-                                            style={{ backgroundImage: "url(Assets/img/App 3.png)" }}
-                                        >
-                                            <a
-                                                href="Assets/Projects/Calculator/index.html"
-                                                target="_blank"
-                                            >
-                                                <button className="btn2" style={{ marginTop: 80 }}>
-                                                    Click
-                                                </button>
-                                            </a>
-                                        </div>
+
                                     </div>
                                 </div>
                             </div>
